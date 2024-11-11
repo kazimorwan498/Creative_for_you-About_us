e6a0344f2af7f104748d8eeff7e311e0f88ebd12
kazimorwan498 <kazimdmorwan498@gmail.com>
2024-11-11 21:24:08 +0600

    create header section

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    darkMode: "class",
+    content: ["./index.html"],
+    theme: {
+        extend: {
+            colors: {
+                redOrange: "rgb(244, 67, 53)",
+                springGreen: "rgb(0,255,132)",
+                bunting: "rgb(45, 49, 66)",
+                salmon: "rgb(255,125,114)",
+            },
+        },
+    },
+    plugins: [],
+};
